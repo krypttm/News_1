bd59d4d18e3266647747c1f4f387821eeed77552
Tatiana <tatianakmml63@gmail.com>
2022-04-08 23:22:48 +0300

    add laravel/ui and install npm, laravel mix

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
